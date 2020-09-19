a6a7bfac5dbb4b03a43e8dd4a8df92725296872c
patricia2008 <pvieirajorge@gmail.com>
2020-09-19 16:39:15 -0300

    Aula de condições
    Gustavo Ganabara Lindo!

---
+++ aulaDom/exe08.js
@@ -0,0 +1,9 @@
+var vel = 60 // velocidade
+ console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if(vel > 60){
+   console.log('Você ultrapassou a velocidade permitida. Multado!');
+}else{
+console.log('Dirija sempres usando cinto de segurança!');
+}
+
+
---
+++ aulaDom/vetor.js
@@ -0,0 +1,8 @@
+var idade = 18
+if(idade >= 16 && idade >= 65){
+    console.log("Voto apcional");
+}else if(idade <= 15 ){
+    console.log('Não vota');
+}else{
+    console.log('Voto obrigatorio');
+}
